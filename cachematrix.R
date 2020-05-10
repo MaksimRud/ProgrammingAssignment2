a86139bc740170d4389a12f8d49593fcd050bde7
Maksym Rud <alice@programmingmentor.com>
2020-05-10 22:27:13 +0300

    first attempt assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        invers <- NULL
+        set <- function(y) {
+          x <<- y
+          invers <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) invers <<- inv
+        getinv <- function() invers
+        list(set = set, get = get,
+             setinv = setmean,
+             getinv = getmean)
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+          message("getting cached data")
+          return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, diag(nrow = nrow(data)), ...)
+        x$setinv(m)
+        inv
 }
